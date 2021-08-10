1238b154d3fe1b03e9faa9705b3a501ca3edf5f3
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-08-10 18:29:37 +0530

    added protocol methods

--- Sources/testFramework/Manager.swift
+++ Sources/testFramework/Manager.swift
@@ -28,7 +28,7 @@ public class Manager {
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
     }
-    public func isValidEmail(_ email: String) {
+    public func isValidEmailWithoutReturn(_ email: String) {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
