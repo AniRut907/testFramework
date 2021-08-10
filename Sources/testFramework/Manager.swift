b84511e7fac5c639d9868ff9e503ebefbf1d08e8
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-08-10 18:23:29 +0530

    added protocol methods

--- Sources/testFramework/Manager.swift
+++ Sources/testFramework/Manager.swift
@@ -6,9 +6,13 @@
 //
 
 import Foundation
+public protocol EmailValidatorDelegate {
+    func result(result:Bool)
+}
 public class Manager {
     
     public init() { }
+    public var emailDelegate: EmailValidatorDelegate?
     
     public func createManager(){
         print("Manager created successfully!")
@@ -24,5 +28,11 @@ public class Manager {
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
     }
-    
+    public func isValidEmail(_ email: String) {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        emailDelegate?.result(result: emailPred.evaluate(with: email))
+        //return emailPred.evaluate(with: email)
+    }
 }
